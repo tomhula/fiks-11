32da5178a2bd593a7f28181caf65678ce995e084
Tomasan7 <tomashula06@gmail.com>
2024-10-10 21:07:59 +0200

    Improve

--- kolo1/posadka/src/commonMain/kotlin/Solver.kt
+++ kolo1/posadka/src/commonMain/kotlin/Solver.kt
@@ -13,8 +13,6 @@ class Solver(val crew: Crew)
     fun solve(): Long
     {
         stabilise(crew)
-//        log{ "STABILISED CREW:" }
-//        log{ crew }
 
         val activeMembers = mutableSetOf<Member>()
         val remainingLeafMembers = leafMembersSorted.toMutableSet()
@@ -32,16 +30,10 @@ class Solver(val crew: Crew)
             }
         }
 
-//        log { "leaf members: ${leafMembersSorted.joinToString { it.signature }}" }
-
         while (true)
         {
-//            log { "active members: ${activeMembers.joinToString { it.signature }}" }
-//            log { "surface members: ${surfaceMembers.joinToString { it.signature }}" }
-//            log { "remaining leaf members: ${remainingLeafMembers.joinToString { it.signature }}" }
-//            log { "weakest points: $weakestPoints" }
-
             var target: Member? = null
+            var targetChild: Member? = null
             var smallestStep: Long? = null
 
             for (leafMember in remainingLeafMembers)
@@ -62,11 +54,10 @@ class Solver(val crew: Crew)
                 if (step != null && (smallestStep == null || step < smallestStep))
                 {
                     target = surfaceMember.parent
+                    targetChild = surfaceMember
                     smallestStep = step
                 }
             }
-
-//            log { "Target is: $target with a step of $smallestStep" }
 
             if (target == null || smallestStep == null || remainingPoints < smallestStep * activeMembers.size || smallestStep * activeMembers.size < 0) // Integer overflow protection
             {
@@ -84,14 +75,14 @@ class Solver(val crew: Crew)
                 remainingLeafMembers.remove(target)
 
             surfaceMembers.add(target)
-            surfaceMembers.removeAll(target.children)
+            if (targetChild != null)
+                surfaceMembers.remove(targetChild)
         }
     }
 
     /** Gives [amount] of points from [remainingPoints] to this member and returns its new total points. */
     private fun Member.givePoints(amount: Long): Long
     {
-//        log { "Giving $amount points to $this [[${remainingPoints - amount}]]" }
         points += amount
         remainingPoints -= amount
         return points
