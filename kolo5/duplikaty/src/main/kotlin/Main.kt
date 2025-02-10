4f5c8bb125d1c98cc3b196aaf006baff9c5cac0b
Tomas Hula <tomashula06@gmail.com>
2025-02-10 16:18:13 +0100

    Duplikaty finally under 2min

--- kolo5/duplikaty/src/main/kotlin/Main.kt
+++ kolo5/duplikaty/src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
+
 fun main()
 {
     val entries = parseInput()
@@ -8,19 +11,39 @@ fun main()
 
 fun countSubstrings(strings: List<String>): Int
 {
-    var count = 0
+    val count = AtomicInteger()
     val appearances = mutableMapOf<String, Int>()
 
-    for ((i, substring) in strings.withIndex())
-    {
-        appearances.putIfAbsent(substring, 0)
-        appearances[substring] = appearances[substring]!! + 1
-        for ((j, string) in strings.withIndex())
-            if (i != j && string.contains(substring))
-                count++
+    val stringsHalf = strings.size / 2
+
+    val t1 = thread {
+        for (i in 0 ..< stringsHalf)
+        {
+            val substring = strings[i]
+            for ((j, string) in strings.withIndex())
+                if (i != j && string.contains(substring))
+                    count.incrementAndGet()
+        }
+    }
+    val t2 = thread {
+        for (i in stringsHalf ..< strings.size)
+        {
+            val substring = strings[i]
+            for ((j, string) in strings.withIndex())
+                if (i != j && string.contains(substring))
+                    count.incrementAndGet()
+        }
+    }
+    val t3 = thread {
+        for (string in strings)
+            appearances[string] = (appearances[string] ?: 0) + 1
     }
 
-    val result = count - appearances.values.sumOf { combinationsOfTwo(it) }
+    t1.join()
+    t2.join()
+    t3.join()
+
+    val result = count.get() - appearances.values.sumOf { combinationsOfTwo(it) }
 
     return result
 }
